b62e3536998d79b4b3caa785a7a47b3d257ba053
wesleylopes <wesleylopes@dcc.ufmg.br>
2017-09-19 15:32:38 -0300

    Adição de dados do nosso gymmer

--- server/models/gymmer.js
+++ server/models/gymmer.js
@@ -9,7 +9,18 @@ const GymmerSchema = new Schema({
 	born: { type: Date, required },
 	cpf: { type: String, min: 11, max: 11, required },
 	cnpj: { type: String, min: 11, max: 18 },
+	height: { type: String, min: 2, max: 7 },
+	Weight: { type: String, min: 2, max: 7 },
 	records: [{ type: Schema.Types.ObjectId, ref: 'records' }],
+	address: {
+		street: { type: String, min: 3, max: 70 },
+		number: { type: String, min: 3, max: 5 },
+		neighborhood: { type: String, min: 3, max: 70 },
+		city: { type: String, min: 3, max: 70 },
+		state: { type: String, min: 3, max: 70 },
+		country: { type: String, min: 3, max: 70 },
+		cep: { type: String, min: 3, max: 70 },
+	 },
 	user: { type: Schema.Types.ObjectId, ref: 'user' },
 	timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
 });
